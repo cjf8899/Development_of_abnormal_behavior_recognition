2b1e1557ead05385c034b3aafd7f670c7e78897f
cjf8899 <53032349+cjf8899@users.noreply.github.com>
2021-02-09 00:39:54 +0900

    Update dataset2d.py

--- dataset/dataset2d.py
+++ dataset/dataset2d.py
@@ -21,19 +21,9 @@ def get_cctv(opt, frame_path, Total_frames, train):
     i = 0
     for a in range(len(path_list)):
         clip.append(path_list[a])
-    
    
     path_len = len(path_list) -1
-   
-
-    if train == 1:
-        ran_idx = np.random.randint(0, path_len)
-    else:
-        ran_idx = np.random.randint(int(len(path_list)/2), int(len(path_list)*3/4) )
-        
-        ran_idx = int(len(path_list)/4) 
-        
-
+    ran_idx = np.random.randint(0, path_len)
     
     return clip[ran_idx]
 
@@ -42,9 +32,9 @@ class CCTV_loader_2d(Dataset):
     def __init__(self, train, opt,transform=None):
         self.transform = transform
         if train == 1:
-            self.clips = glob.glob('/workspace/final_mars/MARS/dataset/action/dataset/train/*/*')
+            self.clips = glob.glob('/workspace/data/train/*/*')
         else:
-            self.clips = glob.glob('/workspace/final_mars/MARS/dataset/action/dataset/test/*/*')
+            self.clips = glob.glob('/workspace/data/test/*/*')
             self.clips.sort()
         self.opt = opt
         self.train_val_test = train
@@ -53,21 +43,20 @@ class CCTV_loader_2d(Dataset):
         return len(self.clips)
     def __getitem__(self, idx):
         path = self.clips[idx]
-        label = path.split('/')[8]
+        label = path.split('/')[4]
         
         if self.train != 1:
-            label_name = path.split('/')[9]
+            label_name = path.split('/')[5]
         else:
             label_name=0
             
         label = label_index.index(label)
         Total_frames = len(glob.glob(path+'/*.jpg'))
-#         print("label : ", label)
         
         clip = get_cctv(self.opt, path, Total_frames, self.train)
-#         print(type(clip))
         image = Image.open(clip).convert("RGB")
-#         print(type(image))
+        
         if self.transform:
             image = self.transform(image)
+            
         return(image, label, label_name)
